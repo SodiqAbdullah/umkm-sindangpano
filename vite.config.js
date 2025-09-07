10335c636d8a2838ee197fb9e46d1c691c6b04c1
Sodiq Abdullah <sodiq.abdullah303@gmail.com>
2025-09-07 15:34:46 +0700

    Fixing Deploy Problem

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(import.meta.env.VITE_SUPABASE_URL || ''),
-    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(import.meta.env.VITE_SUPABASE_ANON_KEY || ''),
+    'process.env': process.env,
   },
 });
